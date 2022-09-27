82ab68b8dca1e9021262ac3e4c63e34cfcaa0c54
Miagato99 <90808722+Miagato99@users.noreply.github.com>
2022-09-27 17:31:03 +0800

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+
+
+import MasterLayout from './layouts/admin/MasterLayout';
+import Home from './components/frontend/Home'
+import Register from './components/frontend/auth/Register'
+import Login from './components/frontend/auth/Login'
+
+import axios from 'axios';
+
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/"
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+
+axios.defaults.withCredentials= true;
+axios.interceptors.request.use(function (config){ 
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+
+
+
+          <Route exact path ="/" component={Home} />
+
+          {/* <Route  path ="/login" component={Login} />
+          <Route  path ="/register" component={Register} /> */}
+          <Route path ="/login">
+            {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Login/>}
+
+          </Route>
+
+          <Route  path ="/register">
+            {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Register/>}
+
+          </Route>
+  
+           <Route path = "/admin" name ="Admin" render = {(props)=> <MasterLayout {...props}/>} />
+
+         
+
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+  
+// );
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// ReactDOM.render (<App/>, document.querySelector("#root"))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
